5256ec83964581e82cb6f52bfb72c1a15e78eee6
Knight <177034@qq.com>
2019-07-09 17:52:03 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import ELEMENT from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-
+Vue.use(ELEMENT)
 Vue.config.productionTip = false
-
+// 职责1 ，导入项目需要依赖的资源（包  ，css等等）
+// 职责2 ，
 new Vue({
   render: h => h(App)
 }).$mount('#app')
