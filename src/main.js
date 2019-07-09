491a43a74748df1aac6bdf610593c11f26cce4d6
Knight <177034@qq.com>
2019-07-09 19:59:31 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
 import App from './App.vue'
 Vue.use(ELEMENT)
 Vue.config.productionTip = false
 // 职责1 ，导入项目需要依赖的资源（包  ，css等等）
 // 职责2 ，
 new Vue({
+  router,
   render: h => h(App)
+
 }).$mount('#app')
