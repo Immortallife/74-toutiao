91e24c50c49630113ddcf3b7402d82e5d93bfd9e
Knight <177034@qq.com>
2019-07-09 21:08:02 +0800

    login首页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        login
+    <div class="login-container">
+        <el-card class="login-box"></el-card>
     </div>
 </template>
 
@@ -11,5 +11,28 @@ export default {
 </script>
 
 <style>
+.login-container{
+    width: 100%;
+    height: 100%;
+    /* background-color: red; */
+    position: absolute;
+    top:0;
+    left:0;
+    background:url("../../assets/images/login_bg.jpg") no-repeat center / cover;
+    }
 
+ .login-box{
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+
+  }
+    img{
+      display: block;
+      width: 200px;
+      margin: 10px auto;
+    }
 </style>
